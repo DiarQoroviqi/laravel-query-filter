0acfbcd277b57d0aa082f236c6859ee83c666af5
diariqoroviqi <qoroviqidiar@gmail.com>
2022-01-24 20:01:28 +0100

    Change default filters naming convention

--- src/Filters/Filterable.php
+++ src/Filters/Filterable.php
@@ -8,11 +8,11 @@ trait Filterable
 {
     public function scopeFilter(Builder $query, $filterClass = null): Builder
     {
-        if (! property_exists($this, 'default_filters') && is_null($filterClass)) {
+        if (! property_exists($this, 'defaultFilters') && is_null($filterClass)) {
             throw new \Exception('please add default_filters property to the model');
         }
 
-        $class = $filterClass ? app($filterClass) : app($this->default_filters);
+        $class = $filterClass ? app($filterClass) : app($this->defaultFilters);
 
         return $class->apply($query);
     }
